a7b8358c22a2155c53e1df710415539bc3515cd2
z17cx <mail@z17.cx>
2022-09-05 20:38:20 +0000

    2022-09-05 20:38:20

--- MW_EXT_Key.i18n.magic.php
+++ MW_EXT_Key.i18n.magic.php
@@ -6,14 +6,14 @@
  * @file
  * @ingroup Extensions
  * @licence GNU GPL
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords = [];
 
 /**
  * English.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['en'] = [
   'key' => [0, 'key'],
@@ -22,7 +22,7 @@ $magicWords['en'] = [
 /**
  * Russian.
  *
- * @author Kitsune Solar
+ * @author z17 Dev
  */
 $magicWords['ru'] = [
   'key' => [0, 'key'],
